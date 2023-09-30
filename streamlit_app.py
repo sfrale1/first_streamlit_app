c830f92f92f69e30b1739c02404236e56a2ff44f
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-09-29 22:01:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 
 my_fruit_list = my_fruit_list.set_index('Fruit');
 
-fruits_selectd = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Apple']);
+fruits_selectd = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),key='Apple');
 fruits_to_show = my_fruit_list.loc[fruits_selected];
 
 #streamlit.dataframe(fruits_to_show);
