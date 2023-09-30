80e92dafa4bbf54be7b607ae60b48783888a96c2
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-09-29 20:29:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ streamlit.title ('My Moms New Healthy Diner');
 
 streamlit.header ('Breakfast Favorites ');
 
-
-
 streamlit.text ('🥣 Omega3 & Blueberry Oatmeal ');
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie ');
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg ');
