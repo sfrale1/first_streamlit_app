d704d4f958f8d90f8192e6a22de96f05a91acd24
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-09-29 20:28:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ streamlit.title ('My Moms New Healthy Diner');
 
 streamlit.header ('Breakfast Favorites ');
 
---  ' 🥣 🥗 🐔 🥑🍞 '
+
 
 streamlit.text ('🥣 Omega3 & Blueberry Oatmeal ');
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie ');
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg ');
 streamlit.text ('🥑🍞 Avocado Toast' );
 
--- streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+-- streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
