da0dc48b382649897255041cf46c2ea7f2ee6abe
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-09-29 21:18:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
+import pandas; 
 import streamlit; 
 streamlit.title ('My Mom\'s New Healthy Diner');
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+
 
 streamlit.header ('Breakfast Favorites ');
 
@@ -9,4 +13,5 @@ streamlit.text ('🐔 Hard-Boiled Free-Range Egg ');
 streamlit.text ('🥑🍞 Avocado Toast' );
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+streamlit.dataframe(my_fruit_list);
 
