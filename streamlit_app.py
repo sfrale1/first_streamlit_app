2278ab2f2a8b7f7dfa202eb1f6238ba4b2ca7a54
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-09-30 21:08:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit');
 
 fruits_selectd = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),  ['Lime','Orange']);
 streamlit.write(fruits_selectd); 
-streamlit.dataframe(fruits_selected);
+
+#streamlit.dataframe(fruits_selected);
 
 streamlit.dataframe(my_fruit_list);
 
