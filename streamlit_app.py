d09445ccea36595422d957b441d508008d9d07f3
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-10-02 09:40:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,8 @@ streamlit.dataframe(my_fruit_list);
 streamlit.header("Fruityvice Fruit Advice!");
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
 #streamlit.text(fruityvice_response);
-streamlit.text(fruityvice_response.json());
+#streamlit.text(fruityvice_response.json());
 
+#Get Json converted to Dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
+streamlit.dataframe(fruityvice_normalized);
