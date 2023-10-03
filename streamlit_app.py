aecbd0bb893400ec9323451b0506ba8003b61f2f
sfrale1 <146461626+sfrale1@users.noreply.github.com>
2023-10-02 21:03:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.dataframe(fruityvice_normalized);
 ##################################
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
 my_cur = my_cnx.cursor();
-my_cur.execute("SELECT Current_timestamp(), CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
+my_cur.execute("select to_varchar(Current_timestamp()) as datetime, CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
 my_data_row = my_cur.fetchone();
 streamlit.text("Hello from Snowflake:");
 streamlit.text(my_data_row);
